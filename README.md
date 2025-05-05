b1323b0180f2dddd1fad76e8e5ac148cdb7b302a
savar95x <skaar513@gmail.com>
2025-05-05 11:54:31 +0530

    demo

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # alien invasion
+A python-pygame personal project to get a practical understanding of OOP 
+## Screenshot
+![demo](images/demo.png)
+## Note
+Temporarily the development paused due to academic commitments
--- alien_invasion.py
+++ alien_invasion.py
@@ -8,7 +8,8 @@ class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        #self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode((1200, 800))
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
